ab41eb2fd59afc7afa5b1c264c062fa863314d87
neustrt23 <anoop22@gmail.com>
2023-02-27 17:48:59 +0530

    Updated code

--- streamlituml.py
+++ streamlituml.py
@@ -34,7 +34,7 @@ if st.button("Generate diagram"):
 
     # extract the generated PlantUML syntax from the API response
     generated_syntax = response.choices[0].text.strip()
-    st.code(generated_syntax) 
+    #st.code(generated_syntax) 
   
     
     #st.text_input("generated_syntax", generated_syntax)
