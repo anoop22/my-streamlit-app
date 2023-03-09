1911656efae08901fe572aceb0b64b903f740358
neustrt23 <anoop22@gmail.com>
2023-03-10 00:08:17 +0530

    commit

--- LearnOS.py
+++ LearnOS.py
@@ -12,7 +12,7 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import ElasticVectorSearch, Pinecone, Weaviate, FAISS
 import os
 import openai
-from google.colab import files
+#from google.colab import files
 
 # initialize the OpenAI API with your API key
 openai.api_key = st.secrets["api_secret"]
