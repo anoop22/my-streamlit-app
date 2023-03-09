9e8658a51c565b371f9277604edbe803a514baed
neustrt23 <anoop22@gmail.com>
2023-03-10 00:33:29 +0530

    commit

--- LearnOS.py
+++ LearnOS.py
@@ -1,88 +1,48 @@
-# !pip install PyPDF2
-# !pip install langchain
-# !pip install python-dotenv
-# !pip install faiss-cpu
-# !pip install tensorflow
-# !pip install openai
-
-# import the modules
-#from PyPDF2 import PdfReader
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.vectorstores import ElasticVectorSearch, Pinecone, Weaviate, FAISS
+import torch
+from transformers import RobertaModel, RobertaTokenizer
+from langchain.vectorstores import FAISS
 import os
-import openai
+import pickle
 import streamlit as st
 
-#from google.colab import files
 
-# initialize the OpenAI API with your API key
-#openai.api_key = st.secrets["api_secret"]
+# Load the RoBERTa model and tokenizer
+model = RobertaModel.from_pretrained('roberta-base')
+tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
 
-
-
-# uploaded_file = files.upload()
-# # Get the file name and path
-# filename = list(uploaded_file.keys())[0]
-# filepath = os.path.abspath(filename)
-
-# reader = PdfReader(filepath)
-# raw_text = ''
-# for i, page in enumerate(reader.pages):
-#     text = page.extract_text()
-#     if text:
-#         raw_text += text
-# text_splitter = CharacterTextSplitter(        
-#     separator = "\n",
-#     chunk_size = 1000,
-#     chunk_overlap  = 200,
-#     length_function = len,
-# )
-# texts = text_splitter.split_text(raw_text)
-
-# import pickle
-# with open('texts.pkl', 'wb') as f:
-#     pickle.dump(texts, f)
-
-# embeddings = OpenAIEmbeddings()
-# import pickle
-# with open("foo.pkl", 'wb') as f:
-#     pickle.dump(embeddings, f)
-# with open("foo.pkl", 'rb') as f:
-#     new_docsearch = pickle.load(f)
-
-
-#import pickle
-#with open("foo.pkl", 'rb') as f:
-#    new_docsearch = pickle.load(f)
-
-openai.api_key = st.secrets["api_secret"]
-
-embeddings = OpenAIEmbeddings()
-
+# Load the texts from a pickle file
 with open('texts.pkl', 'rb') as f:
     texts = pickle.load(f)
 
-docsearch = FAISS.from_texts(texts, embeddings)
+# Define a function to calculate embeddings using the RoBERTa model
+def calculate_embeddings(text):
+    # Tokenize the text
+    tokens = tokenizer.encode(text, add_special_tokens=True)
 
-#query = input("Please ask any question:" )
+    # Convert the tokens to a PyTorch tensor
+    tokens_tensor = torch.tensor([tokens])
 
+    # Calculate the embeddings
+    with torch.no_grad():
+        outputs = model(tokens_tensor)
+        embeddings = outputs[0].numpy()
+
+    return embeddings
+
+# Calculate embeddings for each text and create a FAISS index
+embeddings = [calculate_embeddings(text) for text in texts]
+docsearch = FAISS.from_embeddings(embeddings)
+
+# Get the user query
 query = st.text_input("Please ask any question on OS:" )
 
-docs = docsearch.similarity_search(query)
-
-context = docs[0].page_content
-
-#print(docs[0].page_content)
-
-# from langchain.chains.question_answering import load_qa_chain
-# from langchain.llms import OpenAI
-# chain = load_qa_chain(OpenAI(temperature=0), chain_type="stuff")
-# chain.run(input_documents=docs, question=query)
+# Search for similar texts and get the context for the top result
+docs = docsearch.similarity_search(calculate_embeddings(query))
+context = docs[0].text
 
 # Generate answer using OpenAI API
+openai.api_key = st.secrets["api_secret"]
 response = openai.Completion.create(
-        #engine="gpt-3.5-turbo",
         engine="text-davinci-003",
         prompt=f"I am a Q&A bot. I answer the following question {query} based on the following context:{context}",
         max_tokens=1024,
@@ -90,10 +50,9 @@ response = openai.Completion.create(
         stop=None,
         temperature=0.7,
     )
-    
+
 # Extract the answer from the response
 answer = response.choices[0].text.strip()
- 
+
+# Show the answer in the Streamlit app
 st.text(answer)
- 
-#print (answer)
